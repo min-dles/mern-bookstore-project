12495a14f5d503314072dfe600ad8785ae8b3ba1
mandi andrade <mandi.andrade20@gmail.com>
2023-09-09 18:16:27 -0500

    created first http route w status 200

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,11 @@ import { PORT } from "./config.js";
 
 const app = express();
 
+app.get('/', (request, response) => {
+  console.log('Here is a request:', request);
+  return response.status(200).send('MERN Stack Tutorial - Server is Connected');
+});
+
 app.listen(PORT, () => {
   console.log(`App is listening to port: ${PORT}`);
 });
