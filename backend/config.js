b926ee9166478a098ef46e52aeebafc374997c9b
mandi andrade <mandi.andrade20@gmail.com>
2023-09-11 15:33:02 -0500

    successful db connection and .env set up

--- backend/config.js
+++ backend/config.js
@@ -1,1 +1,7 @@
-export const PORT = 5000;
+// import dotenv files with ES Modules
+import dotenv from 'dotenv';
+dotenv.config({ path: '../.env'} );
+
+// App Set //
+export const PORT = process.env.PORT_NUMBER || 5000;
+export const mongoDBURL = process.env.MONGO_DB_URL || 'mongodb+srv:';
--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 import express from "express";
-import { PORT } from "./config.js";
+import { PORT, mongoDBURL } from "./config.js";
+import mongoose from "mongoose";
 
 const app = express();
 
@@ -8,6 +9,15 @@ app.get('/', (request, response) => {
   return response.status(200).send('MERN Stack Tutorial - Server is Connected');
 });
 
-app.listen(PORT, () => {
-  console.log(`App is listening to port: ${PORT}`);
-});
+mongoose
+  .connect(mongoDBURL)
+  .then(() => {
+    console.log('App connected to DB');
+    app.listen(PORT, () => {
+      console.log(`App is listening to port: ${PORT}`);
+    });
+    console.log('DB and server are both running!');
+  })
+  .catch((error) => {
+    console.log('error w mongoose', error);
+  });
