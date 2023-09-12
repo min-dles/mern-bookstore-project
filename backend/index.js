b3ba2a4163e7e36af434af71fbac494b597acc31
mandi andrade <mandi.andrade20@gmail.com>
2023-09-11 21:56:38 -0500

    added middleware for parsing req body and successfully tested POST

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,41 @@
 import express from "express";
 import { PORT, mongoDBURL } from "./config.js";
 import mongoose from "mongoose";
+import { Book } from './models/bookModel.js';
 
 const app = express();
 
-app.get('/', (request, response) => {
-  console.log('Here is a request:', request);
-  return response.status(200).send('MERN Stack Tutorial - Server is Connected');
+// Middleware for parsing req body:
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  console.log('Here is a request:', req);
+  return res.status(200).send('MERN Stack Tutorial - Server is Connected');
+});
+
+// HTTP route to save a new book, with some data validation: 
+app.post('/books', async (req, res) => {
+  try {
+    if (
+      !req.body.title ||
+      !req.body.author ||
+      !req.body.publishYear
+    ) {
+      return res.status(400).send({
+        message: 'Please provide all required fields: title, author, and year published',
+      });
+    }
+    const newBook = {
+      title: req.body.title,
+      author: req.body.author,
+      publishYear: req.body.publishYear
+    };
+    const book = await Book.create(newBook);
+    return res.status(201).send(book);
+  } catch (error) {
+    console.log(error.message);
+    response.status(500).send({ message: error.message });
+  }
 });
 
 mongoose
