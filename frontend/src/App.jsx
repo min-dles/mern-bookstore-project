63376a0ecb088ed7858caac810fb1933667bbe3c
mandi andrade <mandi.andrade20@gmail.com>
2023-09-19 18:05:01 -0500

    begin implementation of react-router-dom

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,15 @@
 import React from "react";
+import { Routes, Route } from 'react-router-dom';
 
 const App = () => {
   return (
-    <div className='bg-red-400 text-white'>MERN Bookstore App</div>
+    <Routes>
+      <Router path='' element={} />
+      <Router path='' element={} />
+      <Router path='' element={} />
+      <Router path='' element={} />
+      <Router path='' element={} />
+    </Routes>
   )
 }
 
